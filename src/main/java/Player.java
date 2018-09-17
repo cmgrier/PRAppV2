88e1a1cfb0fad3699e58754469438fc172d251e6
cmgrier <cmgrier@wpi.edu>
2018-09-17 13:21:24 -0400

    doesn't change scores, if either player's tag contains "Bye"

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -32,24 +32,28 @@ public class Player implements Comparable<Player>{
         this.initialScore = initialScore;
     }
 
-    //updates the scores of the player object and the opposing player. uses ELO with a K value of 24
+    //updates the scores of the player object and the opposing player. uses ELO with a K value of whatever the default value is
     public void updateScores(Player opponent, boolean won, int K){
-        double R1 = Math.pow(10, this.score / 400.0);
-        double R2 = Math.pow(10, opponent.getScore() / 400.0);
-        double E1 = R1 / (R1 + R2);
-        double E2 = R2 / (R1 + R2);
-        int S1,S2;
-        if(won){
-            S1 = 1;
-            S2 = 0;
-        } else{
-            S1 = 0;
-            S2 = 1;
+        if(this.tag.contains("Bye") || opponent.tag.contains("Bye")){
+
+        }else {
+            double R1 = Math.pow(10, this.score / 400.0);
+            double R2 = Math.pow(10, opponent.getScore() / 400.0);
+            double E1 = R1 / (R1 + R2);
+            double E2 = R2 / (R1 + R2);
+            int S1, S2;
+            if (won) {
+                S1 = 1;
+                S2 = 0;
+            } else {
+                S1 = 0;
+                S2 = 1;
+            }
+            double N1 = this.score + K * (S1 - E1);
+            double N2 = opponent.score + K * (S2 - E2);
+            setScore(N1);
+            opponent.setScore(N2);
         }
-        double N1 = this.score + K * (S1 - E1);
-        double N2 = opponent.score + K * (S2 - E2);
-        setScore(N1);
-        opponent.setScore(N2);
     }
 
     public ArrayList<String> separateCharacters(String characters) {
